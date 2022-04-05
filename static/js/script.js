2e5f2f57fd9ccc846b1e88987a2b5b25c42af66c
VinBdev <vbpod77@gmail.com>
2022-04-05 22:09:17 +0000

    Add categories dropdown

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
-    $('.datepicker').datepicker(
+    $('select').formSelect();
+    $('.datepicker').datepicker({
       format: "dd mmmm, yyyy",
       yearRange: 3,
       showClearBtn: true,
